e4fbd414cf0e4dc25b069bd492a23c99a2f4e525
Matthew Short <smwshort@gmail.com>
2020-02-02 01:08:38 -0600

    Initial

---
+++ DistributionUpdateToolWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DistributionUpdateToolWeb.Startup))]
+namespace DistributionUpdateToolWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
